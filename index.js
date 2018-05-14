3f57010f3b6a9b43942309df25bfc0c3e5610f0f
Mathias Rechtzigel <MathiasaurusRex@gmail.com>
2018-05-13 20:29:49 -0500

    Adding chat message when a user has connected or disconnected

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user has connected');
+    io.emit('chat message', 'a user has connected');
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        io.emit('chat message', 'a user has disconnected');
     })
 
     socket.on('chat message', function(msg){
         console.log('message: ' + msg);
-        
         io.emit('chat message', msg);
     })
 
